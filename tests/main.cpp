0a5f820b2e465700b018ff9e1980726bd9d0bfd4
David Filiks <davidfiliks55@gmail.com>
2023-12-21 15:06:07 +0000

    add CMake and set up project

---
+++ tests/main.cpp
@@ -0,0 +1,12 @@
+﻿// cppturtle.cpp : Defines the entry point for the application.
+//
+
+#include "../cppturtle/cppturtle.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
